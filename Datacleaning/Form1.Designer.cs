22d2b6af4e9bc28aac169aa619c580c257e6f466
AymanMubarak <cs.aymanmubark@gmail.com>
2019-04-28 15:17:57 +0200

    Add files via upload

---
+++ Datacleaning/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Datacleaning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.pB = new System.Windows.Forms.ProgressBar();
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnSearch.Location = new System.Drawing.Point(0, 68);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(407, 41);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Select Folder";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // pB
+            // 
+            this.pB.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pB.Location = new System.Drawing.Point(0, 35);
+            this.pB.Name = "pB";
+            this.pB.Size = new System.Drawing.Size(407, 33);
+            this.pB.Step = 1;
+            this.pB.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Location = new System.Drawing.Point(0, 0);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(407, 35);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "Result";
+            this.txtResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(407, 109);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.pB);
+            this.Controls.Add(this.btnSearch);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ProgressBar pB;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+        private System.Windows.Forms.Label txtResult;
+    }
+}
---
+++ Datacleaning/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datacleaning
+{
+    public partial class Form1 : Form
+    {
+
+        int fileCount;
+        int totalfileCount;
+        List<string> result;
+        string FolderPath = string.Empty;
+        bool Complet = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                FolderPath = FBD.SelectedPath;
+                new Thread(() => Search(FolderPath)).Start();
+            }
+        }
+
+        public void Search(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            totalfileCount = files.Count();
+            pB.Invoke((MethodInvoker)(() => pB.Maximum = totalfileCount));
+            fileCount = 0;
+            result = new List<string>();
+            Parallel.ForEach(files, file =>
+            {
+                new Thread(() => SearchInFile(file)).Start();
+            });
+            Complet = true;
+        }
+
+        public void SearchInFile(string path)
+        {
+            string line = string.Empty;
+            StreamReader file = new StreamReader(path);
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("CARDNUMBER") &&
+                    line.Contains("TERMINALID") &&
+                    line.Contains("CARDNUMBER") &&
+                    line.Contains("CARDSTATUS = I") &&
+                    line.Contains("MOBILENUMBER"))
+                {
+                    if (line.Contains("TERMINALID = 50182001") ||
+                        line.Contains("TERMINALID = 50182002") ||
+                        line.Contains("TERMINALID = 50182003") ||
+                        line.Contains("TERMINALID = 50182004") ||
+                        line.Contains("TERMINALID = 50182005"))
+                    {
+                        string CarPlateNo = line.Split('{')[1].Split(',')[5].Split('=')[1].Trim(' ');
+                        string CardNo = line.Split('{')[1].Split(',')[7].Split('=')[1].Trim(' ');
+                        string MobileNo = line.Split('{')[1].Split(',')[8].Split('=')[1].Trim(' ');
+                        line = " Update SmartCardAccounts set CarPlateNo = '" + CarPlateNo + "' , MobileNo = '" + MobileNo + "' where AccountId = (select s.AccountNo from SmartCards s where s.CardNo = '" + CardNo + "')";
+                        result.Add(line);
+                    }
+                }
+            }
+            addOneThreadComplete();
+        }
+
+        void addOneThreadComplete()
+        {
+            fileCount++;
+            txtResult.Invoke((MethodInvoker)(() => txtResult.Text = "File : " + totalfileCount + " / " + fileCount));
+            pB.Invoke((MethodInvoker)(() => pB.Increment(1)));
+
+            var lineCount = 0;
+            if (Complet && (totalfileCount == fileCount))
+            {
+                pB.Invoke((MethodInvoker)(() => pB.Maximum = result.Count));
+                pB.Invoke((MethodInvoker)(() => pB.Value = 0));
+                
+                FileStream file;
+                using (file = File.Create(FolderPath + "\\Out.sql"))
+                {
+                    foreach (var line in result)
+                    {
+                        lineCount++;
+                        txtResult.Invoke((MethodInvoker)(() => txtResult.Text = "Lines : " + result.Count + " / " + lineCount));
+                        pB.Invoke((MethodInvoker)(() => pB.Increment(1)));
+                        var titile = new UTF8Encoding(false).GetBytes(line + Environment.NewLine);
+                        file.Write(titile, 0, titile.Length);
+                    }
+                }
+                file.Close();
+                txtResult.Invoke((MethodInvoker)(() => txtResult.Text = "Done"));
+                Process.Start(FolderPath + "\\Out.sql");
+            }
+        }
+    }
+}
